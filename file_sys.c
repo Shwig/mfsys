226652be09b47f935d2a0c13e9eefecc9d842aef
Shwig <omara.matthew1@gmail.com>
2019-04-26 23:45:17 -0400

    Added and munmap
    compiled with CFLAGS -g -std=gnu99 -Wall -Werror -Wextra
    No errors, runs.

--- file_sys.c
+++ file_sys.c
@@ -7,17 +7,27 @@ int main(void) {
 
   // open the disk file with r/w permission, creat if not exists
   int disk_fd = open("mf_disk.bin", O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
-  if (disk_fd<0) { perror("mf_disk.bin open"); exit(EXIT_FAILURE); };
+  if (disk_fd<0) { perror("mf_disk.bin open"); return(EXIT_FAILURE); };
 
   // get disk file meta data
-  struct stat Disk_stat = {};
-  if (fstat(disk_fd,&Disk_stat)) { perror("fstat"); exit(EXIT_FAILURE); };
+  // struct stat Disk_stat = {};
+  // if (fstat(disk_fd,&Disk_stat)) { perror("fstat failed"); exit(EXIT_FAILURE); };
 
+  // map file to file_sys virtual space
   void *disk_map = mmap(NULL, page_size, PROT_READ|PROT_WRITE,
                                                   MAP_SHARED, disk_fd, 0);
-  if (disk_map == MAP_FAILED) { perror("mmap"); exit(EXIT_FAILURE); };
+  if (disk_map == MAP_FAILED) { perror("mmap failed"); return(EXIT_FAILURE); };
 
-  close(disk_fd);
+  // close disk_fd text file descriptor, use later for operations on disk_map
+  if (close(disk_fd)) { perror("disk_fd close faild"); return(EXIT_FAILURE); };
+
+  //partition file into FAT and DataBlock segemnts
+  /* ?????  */
+
+
+
+  // unmap the disk file
+  if (munmap(NULL, page_size)) { perror("munmap failed"); return(EXIT_FAILURE); };
 
   return EXIT_SUCCESS;
 }
