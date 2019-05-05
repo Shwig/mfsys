9166eb0502857146c36f1838ea0e5ebb9f16b65e
Shwig <omara.matthew1@gmail.com>
2019-05-05 03:17:31 -0400

    maped fat_table and data section to file

--- file_sys.c
+++ file_sys.c
@@ -6,19 +6,23 @@ int main(void) {
   long page_size = sysconf(_SC_PAGE_SIZE);
 
   // open the disk file with r/w permission, creat if not exists
-  int disk_fd = open("mf_disk.bin", O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
-  if (disk_fd<0) { perror("mf_disk.bin open"); return(EXIT_FAILURE); };
+  int disk_fd = open(DEFAULT_STORAGE_FILE, O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
+  if (disk_fd<0) { perror("DEFAULT_STORAGE_FILE open"); close(disk_fd); return(EXIT_FAILURE); };
 
   // get disk file meta data
   // struct stat Disk_stat = {};
   // if (fstat(disk_fd,&Disk_stat)) { perror("fstat failed"); exit(EXIT_FAILURE); };
 
   // map file to file_sys virtual space
-  void *disk_map = mmap(NULL, page_size, PROT_READ|PROT_WRITE, MAP_SHARED, disk_fd, 0);
-  if (disk_map == MAP_FAILED) { perror("mmap failed"); return(EXIT_FAILURE); };
+  // void *disk_map = mmap(NULL, page_size, PROT_READ|PROT_WRITE, MAP_SHARED, disk_fd, 0);
+  // if (disk_map == MAP_FAILED) { perror("mmap failed"); close(disk_fd); return(EXIT_FAILURE); };
+  Fat *fat_table = (Fat *) mmap(NULL, page_size, PROT_READ|PROT_WRITE, MAP_SHARED, disk_fd, 0);
+  if (fat_table == MAP_FAILED) { perror("mmap failed"); close(disk_fd); return(EXIT_FAILURE); };
+  Block *blocks =(Fat *)(fat_table + DEFAULT_OFFSET);
 
   // close disk_fd text file descriptor, use later for operations on disk_map
   if (close(disk_fd)) { perror("disk_fd close faild"); return(EXIT_FAILURE); };
+  disk_fd = 0;
 
   //partition file into FAT and DataBlock segemnts
   /* ?????  */
@@ -26,7 +30,7 @@ int main(void) {
 
 
   // unmap the disk file
-  if (munmap(NULL, page_size)) { perror("munmap failed"); return(EXIT_FAILURE); };
+  if (munmap(fat_table, page_size)) { perror("munmap failed"); return(EXIT_FAILURE); };
 
   return EXIT_SUCCESS;
 }
